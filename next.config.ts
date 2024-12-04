3f6b3bdd8e487704a1a32e072e2b2155d6f352e9
vladchashush <vladchashush@gmail.com>
2024-12-04 12:30:34 +0400

    v0.0.2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: 'export',
-	distDir: '/dist'
+	distDir: '/docs'
 }
 
 export default nextConfig
