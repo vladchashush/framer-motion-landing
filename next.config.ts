74730c8f79a8eee31cdc812fdcd8ceace89d42e9
vladchashush <vladchashush@gmail.com>
2024-12-04 13:37:53 +0400

    v0.0.5

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: 'export',
-	basePath: '/framer-motion-landing/docs',
+	basePath: '/framer-motion-landing/docs/',
 	distDir: '/docs',
 	images: {
 		unoptimized: true
